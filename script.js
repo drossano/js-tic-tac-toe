1310fb67e304d375c3a7e15d776d1ba3c1157a3e
Dean <72749793+drossano@users.noreply.github.com>
2023-07-27 14:10:38 -0400

    Display markers using JS

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const gameBoard = (() => {
-  const boardArray = [];
-  return { boardArray };
+  const boardArray = ["X", "O", "X", "O", "X", "O", "X", "O", "X"];
+  const displayBoard = () => {
+    const board = document.getElementById("board");
+    const spaces = board.children;
+    for (let i = 0; i < spaces.length; i++) {
+      const space = spaces[i];
+      space.textContent = boardArray[i];
+    }
+  };
+  return { boardArray, displayBoard };
 })();
 
 const Player = (marker) => {};
 
 const gameController = (() => {})();
+gameBoard.displayBoard();
